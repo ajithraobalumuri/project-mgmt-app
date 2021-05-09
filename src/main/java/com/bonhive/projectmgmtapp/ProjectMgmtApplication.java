31dce6f5b4d6f8516145db766cd057ee0a2d74da
ajith <ajithraobalumuri@gmail.com>
2021-05-09 07:03:27 +0200

    first commit

---
+++ src/main/java/com/bonhive/projectmgmtapp/ProjectMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.bonhive.projectmgmtapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMgmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMgmtApplication.class, args);
+    }
+
+}
